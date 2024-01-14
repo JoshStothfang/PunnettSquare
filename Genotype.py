0d950065d4dc4ba99c5097275193370b6ca06491
Josh Stothfang <josh@joshstothfang.com>
2024-01-14 16:11:00 -0500

    Added alleles and couplings variables to Genotype class

--- Genotype.py
+++ Genotype.py
@@ -1,2 +1,17 @@
 class Genotype:
-    pass
+    def __init__(self, input):
+        self.input = input
+        self.alleles = list(input)
+        self.couplings = self.generateCouplings(self.alleles)
+    
+    def generateCouplings(self, alleles):
+        couplings = []
+        couplingIndices = ["024", "025", "034", "035", "124", "125", "134", "135"]
+
+        for indices in couplingIndices:
+            coupling = ""
+            for index in indices:
+                coupling += alleles[int(index)]
+            couplings.append(coupling)
+        
+        return couplings
--- PunnettSquare.py
+++ PunnettSquare.py
@@ -1,1 +1,8 @@
 import sys
+from Genotype import Genotype
+
+parent1 = Genotype("RrYyWw")
+parent2 = Genotype("RRYYWW")
+
+print(parent1.couplings)
+print(parent2.couplings)
