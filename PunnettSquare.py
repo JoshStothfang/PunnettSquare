d89a84ff11c93c8f891027d28f31af5c6934624d
Josh Stothfang <josh@joshstothfang.com>
2024-01-14 16:36:19 -0500

    Generates all children genotypes of parent genotypes

--- PunnettSquare.py
+++ PunnettSquare.py
@@ -4,5 +4,18 @@ from Genotype import Genotype
 parent1 = Genotype("RrYyWw")
 parent2 = Genotype("RRYYWW")
 
-print(parent1.couplings)
-print(parent2.couplings)
+children = []
+
+def breed(coupling1, coupling2):
+    child = ""
+    for i in range(3):
+        child += coupling1[i]
+        child += coupling2[i]
+    return child
+
+for coupling1 in parent1.couplings:
+    for coupling2 in parent2.couplings:
+        children.append(breed(coupling1, coupling2))
+
+print(children)
+print(len(children))
