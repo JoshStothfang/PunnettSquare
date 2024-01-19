a6a589f5e3b189771ffb1dcbb27821aac1b564f3
Josh Stothfang <josh@joshstothfang.com>
2024-01-19 17:01:37 -0500

    Added main() function

--- PunnettSquare.py
+++ PunnettSquare.py
@@ -2,10 +2,32 @@ import sys
 from Genotype import Genotype
 from collections import Counter
 
-parent1 = Genotype(sys.argv[1])
-parent2 = Genotype(sys.argv[2])
+def main():
+    parent1 = Genotype(sys.argv[1])
+    parent2 = Genotype(sys.argv[2])
 
-children = []
+    children = []
+
+    for coupling1 in parent1.couplings:
+        for coupling2 in parent2.couplings:
+            children.append(breed(coupling1, coupling2))
+
+    numericChildren = []
+
+    for child in children:
+        numericChildren.append(Genotype.convertToNumeric(child))
+
+    outputTuples = []
+
+    for numericChild in list(Counter(numericChildren)):
+        genotypeChild = Genotype.convertToGenotype(numericChild)
+        percentage = (Counter(numericChildren).get(numericChild))/64
+        outputTuples.append((percentage, genotypeChild, numericChild))
+
+    outputTuples.sort(reverse=True)
+
+    for percent, geno, _ in outputTuples:
+        print(str(percent * 100) + "%", geno, sep="\t")
 
 def breed(coupling1, coupling2):
     child = ""
@@ -14,23 +36,5 @@ def breed(coupling1, coupling2):
         child += coupling2[i]
     return child
 
-for coupling1 in parent1.couplings:
-    for coupling2 in parent2.couplings:
-        children.append(breed(coupling1, coupling2))
-
-numericChildren = []
-
-for child in children:
-    numericChildren.append(Genotype.convertToNumeric(child))
-
-outputTuples = []
-
-for numericChild in list(Counter(numericChildren)):
-    genotypeChild = Genotype.convertToGenotype(numericChild)
-    percentage = (Counter(numericChildren).get(numericChild))/64
-    outputTuples.append((percentage, genotypeChild, numericChild))
-
-outputTuples.sort(reverse=True)
-
-for percent, geno, _ in outputTuples:
-    print(str(percent * 100) + "%", geno, sep="\t")
+if __name__ == "__main__":
+    main()
