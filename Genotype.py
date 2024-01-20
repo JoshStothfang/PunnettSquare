63ea2875197eef9532bb50e8c3155822c44ea5c3
Josh Stothfang <josh@joshstothfang.com>
2024-01-20 13:32:10 -0500

    Removed convertToNumeric, convertToGenotype, and __convertPair methods as they are no longer needed

--- Genotype.py
+++ Genotype.py
@@ -15,27 +15,3 @@ class Genotype:
             alleleCombos.append(alleleCombo)
         
         return alleleCombos
-    
-    def __convertPair(inputPair, letter = "X"):
-        if inputPair in ["rr", "yy", "ww"]:
-            return "0"
-        elif inputPair in ["Rr", "Yy", "Ww", "rR", "yY", "wW"]:
-            return "1"
-        elif inputPair in ["RR", "YY", "WW"]:
-            return "2"
-        elif inputPair == "0":
-            return letter.lower() + letter.lower()
-        elif inputPair == "1":
-            return letter.upper() + letter.lower()
-        elif inputPair == "2":
-            return letter.upper() + letter.upper()
-        else:
-            return -1
-
-    @classmethod
-    def convertToNumeric(self, genotype):
-        return self.__convertPair(genotype[0:2]) + " - " + self.__convertPair(genotype[2:4]) + " - " + self.__convertPair(genotype[4:])
-    
-    @classmethod
-    def convertToGenotype(self, numeric):
-        return self.__convertPair(numeric[0:1], "R") + self.__convertPair(numeric[4:5], "Y") + self.__convertPair(numeric[8:], "W")
