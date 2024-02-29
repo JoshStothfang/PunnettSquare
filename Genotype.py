edd3953981ff93e5c4a9c192780baeae1de711d4
Josh Stothfang <josh@joshstothfang.com>
2024-02-29 14:20:56 -0500

    Added comments

--- Genotype.py
+++ Genotype.py
@@ -7,12 +7,14 @@ class Genotype:
     
     # Generates allele combos for axis of punnett square.
     def alleleCombosFactory(self, alleles):
+        # Creates alleleCombos array containing 2 arrays. One containing the last allele in genotype. The other containing the second-to-last allele in genotype.
         countdown = len(alleles) - 1
         alleleCombos = [[alleles[countdown]]]
         countdown -= 1
         alleleCombos.insert(0, [alleles[countdown]])
         countdown -= 1
 
+        # Duplicates alleleCombos array on itself and alternates between the previous two alleles adding them to the beginning of the child arrays.
         while countdown > 0:
 
             newAlleleCombos = deepcopy(alleleCombos) + deepcopy(alleleCombos)
